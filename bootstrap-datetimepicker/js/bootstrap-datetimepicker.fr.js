07557dcd327e4bb5c1ab6fb8f3e1a8bb3e795e66
Demy <18911887579@sina.cn>
2015-12-21 10:51:20 +0800

    bootstrap-date-timepicker日期控件初始值
    可修改配置

---
+++ bootstrap-datetimepicker/js/bootstrap-datetimepicker.fr.js
@@ -0,0 +1,20 @@
+/**
+ * French translation for bootstrap-datetimepicker
+ * Nico Mollet <nico.mollet@gmail.com>
+ */
+;(function($){
+	$.fn.datetimepicker.dates['fr'] = {
+		days: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"],
+		daysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"],
+		daysMin: ["D", "L", "Ma", "Me", "J", "V", "S", "D"],
+		//months: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+		//months: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
+		months: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
+		monthsShort: ["Jan", "Fev", "Mar", "Avr", "Mai", "Jui", "Jul", "Aou", "Sep", "Oct", "Nov", "Dec"],
+		today: "Aujourd'hui",
+		suffix: [],
+		meridiem: ["am", "pm"],
+		weekStart: 1,
+		format: "yyyy/mm/dd hh:ii"
+	};
+}(jQuery));
